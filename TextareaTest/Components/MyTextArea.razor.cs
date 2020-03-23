881c14b2ea84ea867386d24d8063ec4903e4a02e
Benjamin Vertonghen <benjamin.vertonghen@apped.be>
2020-03-23 04:30:43 +0100

    Add project files.

---
+++ TextareaTest/Components/MyTextArea.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace TextareaTest.Components
+{
+    partial class MyTextArea
+    {
+        [Parameter] public string Value { get; set; } = string.Empty;
+        [Parameter] public EventCallback<string> ValueChanged { get; set; }
+        RenderFragment copyElement;
+        void AutoResize(KeyboardEventArgs args)
+        {
+            copyElement = new RenderFragment(b =>
+            {
+                b.OpenElement(1, "div");
+                b.AddContent(2, (MarkupString)Value.Replace("\n", "<br/>"));
+                b.CloseElement();
+            });
+        }
+    }
+}
